01cf5dfaa5c06976eca181829f56e4d2f02696b0
Arthur Melkonyan <arthurmelkonyan@gmail.com>
2020-09-08 12:11:34 +0400

    Inital code commit

---
+++ src/ArDiCacheManager/CachingDefaults.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArDiCacheManager
+{
+    /// <summary>
+    /// Represents default values related to caching
+    /// </summary>
+    public static partial class CachingDefaults
+    {
+        /// <summary>
+        /// Gets the default cache time in minutes
+        /// </summary>
+        public static int CacheTime => 60;
+    }
+}
---
+++ src/TestConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using ArDiCacheManager;
+using ArDiCacheManager.MemoryCache;
+using System;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cache = new Microsoft.Extensions.Caching.Memory.MemoryCache(new Microsoft.Extensions.Caching.Memory.MemoryCacheOptions());
+
+            IArDiCacheManager cacheManager = new ArDiMemoryCacheManager(cache);
+            var key = new CacheKey("mycacheitem");
+            var result = cacheManager.Get<string>(key, () =>
+            {
+
+                return "Hello from cacge";
+            });
+
+            var result2 = cacheManager.Get<string>(key, () =>
+            {
+                return "Hello from cacge";
+            });
+        }
+    }
+}
